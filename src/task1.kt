e2e7162b7cd0b5633bd607da576f9a54182e4234
mainovmx <49530238+mainovmx@users.noreply.github.com>
2021-09-29 13:20:27 +0700

    Delete src directory

--- src/task1.kt
+++
@@ -1,7 +0,0 @@
-fun main() {
-    val list  = readLine()?.split(" ")?.toList()?:return
-    println("Вывод списка")
-    list.forEach{
-            el -> println(el)
-    }
-}
--- src/task2.kt
+++
@@ -1,8 +0,0 @@
-fun main() {
-    val list  = readLine()?.split(" ")?.toList()?:return
-    val sortList = list.sorted()
-    println("Сортируем")
-    sortList.forEach{
-            el -> println(el)
-    }
-}
