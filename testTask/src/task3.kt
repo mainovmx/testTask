ba1f878cb9b5deb8058f3a7a6d5c8fb120ef3b54
mainovmx <49530238+mainovmx@users.noreply.github.com>
2021-10-16 01:30:12 +0700

    Delete testTask directory

--- testTask/src/task3.kt
+++
@@ -1,9 +0,0 @@
-fun main() {
-    val list  = readLine()?.split(" ")?.toList() ?:return
-    val sortList = list.sorted()
-    val uniqueVal = sortList.toSet()
-    println("Выводим уникальные значения")
-    uniqueVal.forEach{
-            el -> println(el)
-    }
-}
