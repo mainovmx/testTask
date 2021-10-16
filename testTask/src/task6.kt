93af58a9c1b2931218d3bca79d5ce02db908082b
mainovmx <49530238+mainovmx@users.noreply.github.com>
2021-10-16 11:59:20 +0700

    #1 3,4,5,6,7,8

--- testTask/src/task6.kt
+++ testTask/src/task6.kt
@@ -1,5 +1,5 @@
 fun main(args:Array<String>) {
-    val list  = if (args.isEmpty) readLine()?.split(" ")?.toList() ?:return else args.toList()
+    val list  = if (args.isEmpty()) readLine()?.split(" ")?.toList() ?:return else args.toList()
     val sortList = list.sorted()
     val countVal = sortList.groupingBy { it }.eachCount().toSortedMap()
     val sortCountUnique = countVal.toList().sortedByDescending { (_, value) -> value }.toMap()
