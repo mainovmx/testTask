fbfd54d3f940a635a9092547ee097120ad321804
mainovmx <49530238+mainovmx@users.noreply.github.com>
2021-11-11 21:03:33 +0700

    add function

--- src/task6.kt
+++ src/task6.kt
@@ -1,7 +1,10 @@
 fun main(args: Array<String>) {
-    if (args.isEmpty())
-        readLine()?.split(" ")?.toList()?.sorted()?.groupingBy { it }?.eachCount()?.toSortedMap()?.toList()
-            ?.sortedByDescending { (_, value) -> value }?.toMap()?.forEach(::println)
-    else args.toList().sorted().groupingBy { it }.eachCount().toSortedMap().toList()
-        .sortedByDescending { (_, value) -> value }.toMap().forEach(::println)
+    val a: List<String>? = if (args.isEmpty()) readLine()?.split(" ")?.toList()
+    else args.toList()
+    out(a)
+}
+
+fun out(args: List<String>?) {
+    args?.toList()?.sorted()?.groupingBy { it }?.eachCount()?.toSortedMap()?.toList()
+        ?.sortedByDescending { (_, value) -> value }?.toMap()?.forEach(::println)
 }
